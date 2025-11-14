075ebb9aac99464af2a1bdafa02a8f24fbdb22e9
ren1989lisa <renata.ortizchavez19@utez.edu.mx>
2025-11-13 19:24:09 -0600

    interfases

--- src/main/java/mx/edu/utez/sistemadegestiondeprocutos/Main.java
+++ src/main/java/mx/edu/utez/sistemadegestiondeprocutos/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/mx/edu/utez/sistemadegestiondeprocutos/hello-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/mx/edu/utez/sistemadegestiondeprocutos/auth/Products.fxml"));
 
             if (fxmlLoader.getLocation() == null) {
                 return;
---
+++ src/main/java/mx/edu/utez/sistemadegestiondeprocutos/controllers/AddProductController.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.sistemadegestiondeprocutos.controllers;
+
+public class AddProductController {
+}
